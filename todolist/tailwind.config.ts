da982d2b385cc579595bad432ced0dec520ac620
noveril <pitterpatterpik@gmail.com>
2024-12-01 23:37:42 +0900

    TailwindCSS 설정 추가

--- todolist/tailwind.config.ts
+++ todolist/tailwind.config.ts
@@ -11,7 +11,40 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slate: {
+          900: "#0f172a",
+          800: "#1e293b",
+          500: "#64748b",
+          400: "#94a3b8",
+          300: "#cbd5e1",
+          200: "#e2e8f0",
+          100: "#f1f5f9",
+        },
+        violet: {
+          600: "#7c3aed",
+          100: "#ede9fe",
+        },
+        rose: {
+          500: "#f43f5e",
+        },
+        lime: {
+          300: "#bef264",
+        },
+        amber: {
+          800: "#92400e",
+        },
       },
+    },
+    fontSize: {
+      small: "16px",
+      medium: "18px",
+      large: "20px",
+    },
+    fontWeight: {
+      bold: "700",
+    },
+    fontFamily: {
+      nanum: ["NanumSquare", "sans-serif"],
     },
   },
   plugins: [],
