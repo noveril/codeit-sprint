64a45d2ab0812c34dd4949ba91ce3c2e8d4266c2
noveril <pitterpatterpik@gmail.com>
2024-12-07 18:31:37 +0900

    layout 수정

--- todolist/app/layout.tsx
+++ todolist/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { TodoProvider } from "@/src/contexts/TodoContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <TodoProvider>{children}</TodoProvider>
       </body>
     </html>
   );
